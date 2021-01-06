85fd1f816c032ba079fc322ce89670c7813848eb
ShivanshiCoder <75568969+ShivanshiCoder@users.noreply.github.com>
2021-01-06 16:37:03 +0530

    Add files via upload

---
+++ Receipe.js
@@ -0,0 +1,21 @@
+
+Step 1: Stir Together Dry Ingredients;
+Step2: Allow Ingredients to Reach Room Temperature;
+Step3: Combine the Butter and Sugar;
+Step4: Add Eggs One at a Time;
+Make a well in the center of the bowl. oil, vanillaExtract & milk; 
+Step5: Alternate Adding Dry and Wet Ingredients;
+Step6: Pour Batter into Pans;
+Step7: Prepare Baking Pans;
+ Pour this to the cake pans; 
+Step8: Preheat the Oven;
+Bake at 175 C for 28 to 30 mins or until the tester inserted comes out clean;
+Cool the cake in the pans on a wire rack for 10 mins;
+
+
+
+
+
+
+
+
---
+++ ingredients.js
@@ -0,0 +1,2 @@
+Cocoa,butter,milk,sugar,flour,dryFruits,chocolate,bakingSoda,bakingPowder,Oil;
+vanillaExtract,eggs;
